98aeceb98b9a0805bedca24fb141dc032884a4ba
marenger <marenger@student.matnat.uio.no>
2014-07-10 12:56:22 +0200

    Create setup.py

---
+++ doc/src/project/src/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy as np
+
+cymodule = 'wave2D_u0_cy'
+setup(
+name=cymodule,
+ext_modules=[Extension(cymodule, [cymodule + '.pyx'],include_dirs=[np.get_include()])],
+cmdclass={'build_ext': build_ext},
+)
