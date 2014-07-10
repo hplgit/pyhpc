99e077a9ee37265e3d1bb6d997e9e0d342617b44
marenger <marenger@student.matnat.uio.no>
2014-07-10 12:55:09 +0200

    Create wave2D_u0_cython_test.py

---
+++ doc/src/project/src/wave2D_u0_cython_test.py
@@ -0,0 +1,218 @@
+import time
+from numpy import *
+
+def solver(I, V, f, c, Lx, Ly, Nx, Ny, dt, T,
+           user_action=None, version='scalar'):
+    if version == 'cython':
+        try:
+            #import pyximport; pyximport.install()
+            import wave2D_u0_cy as compiled_loops
+            advance = compiled_loops.advance
+        except ImportError as e:
+            print 'No module wave2D_u0_loop_cy. Run make_wave2D.sh!'
+            print e
+            sys.exit(1)
+    elif version == 'f77':
+        try:
+            import wave2D_u0_loop_f77 as compiled_loops
+            advance = compiled_loops.advance
+        except ImportError:
+            print 'No module wave2D_u0_loop_f77. Run make_wave2D.sh!'
+            sys.exit(1)
+    elif version == 'c_f2py':
+        try:
+            import wave2D_u0_loop_c_f2py as compiled_loops
+            advance = compiled_loops.advance
+        except ImportError:
+            print 'No module wave2D_u0_loop_c_f2py. Run make_wave2D.sh!'
+            sys.exit(1)
+    elif version == 'c_cy':
+        try:
+            import wave2D_u0_loop_c_cy as compiled_loops
+            advance = compiled_loops.advance_cwrap
+        except ImportError as e:
+            print 'No module wave2D_u0_loop_c_cy. Run make_wave2D.sh!'
+            print e
+            sys.exit(1)
+    elif version == 'vectorized':
+        advance = advance_vectorized
+    elif version == 'scalar':
+        advance = advance_scalar
+
+    x = linspace(0, Lx, Nx+1)  # mesh points in x dir
+    y = linspace(0, Ly, Ny+1)  # mesh points in y dir
+    dx = x[1] - x[0]
+    dy = y[1] - y[0]
+
+    xv = x[:,newaxis]          # for vectorized function evaluations
+    yv = y[newaxis,:]
+
+    stability_limit = (1/float(c))*(1/sqrt(1/dx**2 + 1/dy**2))
+    if dt <= 0:                # max time step?
+        safety_factor = -dt    # use negative dt as safety factor
+        dt = safety_factor*stability_limit
+    elif dt > stability_limit:
+        print 'error: dt=%g exceeds the stability limit %g' % \
+              (dt, stability_limit)
+    Nt = int(round(T/float(dt)))
+    t = linspace(0, Nt*dt, Nt+1)    # mesh points in time
+    Cx2 = (c*dt/dx)**2;  Cy2 = (c*dt/dy)**2    # help variables
+
+    # Allow f and V to be None or 0
+    if f is None or f == 0:
+        f = (lambda x, y, t: 0) if version == 'scalar' else \
+            lambda x, y, t: zeros((x.shape[0], y.shape[1]))
+        # or simpler: x*y*0
+    if V is None or V == 0:
+        V = (lambda x, y: 0) if version == 'scalar' else \
+            lambda x, y: zeros((x.shape[0], y.shape[1]))
+
+
+    order = 'Fortran' if version == 'f77' else 'C'
+    u   = zeros((Nx+1,Ny+1), order=order)   # solution array
+    u_1 = zeros((Nx+1,Ny+1), order=order)   # solution at t-dt
+    u_2 = zeros((Nx+1,Ny+1), order=order)   # solution at t-2*dt
+    f_a = zeros((Nx+1,Ny+1), order=order)   # for compiled loops
+
+    Ix = range(0, u.shape[0])
+    Iy = range(0, u.shape[1])
+    It = range(0, t.shape[0])
+    import time; t0 = time.clock()          # for measuring CPU time
+
+    # Load initial condition into u_1
+    if version == 'scalar':
+        for i in Ix:
+            for j in Iy:
+                u_1[i,j] = I(x[i], y[j])
+    else: # use vectorized version
+        u_1[:,:] = I(xv, yv)
+    
+    if user_action is not None:
+        user_action(u_1, x, xv, y, yv, t, 0)
+
+    # Special formula for first time step
+    n = 0
+    # Can use advance function with adjusted parameters (note: u_2=0)
+    if version == 'scalar':
+        u = advance(u, u_1, u_2, f, x, y, t, n,
+                    Cx2, Cy2, dt, V, step1=True)
+
+    else:  # use vectorized version
+        f_a[:,:] = f(xv, yv, t[n])  # precompute, size as u
+        V_a = V(xv, yv)
+        u = advance_vectorized(u, u_1, u_2, f_a, Cx2, Cy2, dt, V=V_a, step1=True)
+
+    if user_action is not None:
+        user_action(u, x, xv, y, yv, t, 1)
+
+    u_2[:,:] = u_1; u_1[:,:] = u
+
+    for n in It[1:-1]:
+        if version == 'scalar':
+            # use f(x,y,t) function
+            u = advance(u, u_1, u_2, f, x, y, t, n, Cx2, Cy2, dt)
+        else:
+            f_a[:,:] = f(xv, yv, t[n])  # precompute, size as u
+            u = advance(u, u_1, u_2, f_a, Cx2, Cy2, dt)
+
+        if version == 'f77':
+            for a in 'u', 'u_1', 'u_2', 'f_a':
+                if not isfortran(eval(a)):
+                    print '%s: not Fortran storage!' % a
+
+        if user_action is not None:
+            if user_action(u, x, xv, y, yv, t, n+1):
+                break
+
+        u_2[:,:], u_1[:,:] = u_1, u
+
+    t1 = time.clock()
+    # dt might be computed in this function so return the value
+    return dt, t1 - t0
+
+def advance_scalar(u, u_1, u_2, f, x, y, t, n, Cx2, Cy2, dt,
+                   V=None, step1=False):
+    Ix = range(0, u.shape[0]);  Iy = range(0, u.shape[1])
+    dt2 = dt**2
+    if step1:
+        Cx2 = 0.5*Cx2;  Cy2 = 0.5*Cy2; dt2 = 0.5*dt2
+        D1 = 1;  D2 = 0
+    else:
+        D1 = 2;  D2 = 1
+    for i in Ix[1:-1]:
+        for j in Iy[1:-1]:
+            u_xx = u_1[i-1,j] - 2*u_1[i,j] + u_1[i+1,j]
+            u_yy = u_1[i,j-1] - 2*u_1[i,j] + u_1[i,j+1]
+            u[i,j] = D1*u_1[i,j] - D2*u_2[i,j] + \
+                     Cx2*u_xx + Cy2*u_yy + dt2*f(x[i], y[j], t[n])
+            if step1:
+                u[i,j] += dt*V(x[i], y[j])
+    # Boundary condition u=0
+    j = Iy[0]
+    for i in Ix: u[i,j] = 0
+    j = Iy[-1]
+    for i in Ix: u[i,j] = 0
+    i = Ix[0]
+    for j in Iy: u[i,j] = 0
+    i = Ix[-1]
+    for j in Iy: u[i,j] = 0
+    return u
+
+def advance_vectorized(u, u_1, u_2, f_a, Cx2, Cy2, dt,
+                       V=None, step1=False):
+    dt2 = dt**2
+    if step1:
+        Cx2 = 0.5*Cx2;  Cy2 = 0.5*Cy2; dt2 = 0.5*dt2
+        D1 = 1;  D2 = 0
+    else:
+        D1 = 2;  D2 = 1
+    u_xx = u_1[:-2,1:-1] - 2*u_1[1:-1,1:-1] + u_1[2:,1:-1]
+    u_yy = u_1[1:-1,:-2] - 2*u_1[1:-1,1:-1] + u_1[1:-1,2:]
+    u[1:-1,1:-1] = D1*u_1[1:-1,1:-1] - D2*u_2[1:-1,1:-1] + \
+                   Cx2*u_xx + Cy2*u_yy + dt2*f_a[1:-1,1:-1]
+    if step1:
+        u[1:-1,1:-1] += dt*V[1:-1, 1:-1]
+    # Boundary condition u=0
+    j = 0
+    u[:,j] = 0
+    j = u.shape[1]-1
+    u[:,j] = 0
+    i = 0
+    u[i,:] = 0
+    i = u.shape[0]-1
+    u[i,:] = 0
+    return u
+
+import nose.tools as nt
+
+def test_quadratic(Nx=4, Ny=5):
+    def exact_solution(x, y, t):
+        return x*(Lx - x)*y*(Ly - y)*(1 + 0.5*t)
+
+    def I(x, y):
+        return exact_solution(x, y, 0)
+
+    def V(x, y):
+        return 0.5*exact_solution(x, y, 0)
+
+    def f(x, y, t):
+        return 2*c**2*(1 + 0.5*t)*(y*(Ly - y) + x*(Lx - x))
+
+    Lx = 3;  Ly = 3
+    c = 1.5
+    dt = -1 # use longest possible steps
+    T = 18
+
+    def assert_no_error(u, x, xv, y, yv, t, n):
+        u_e = exact_solution(xv, yv, t[n])
+        diff = abs(u - u_e).max()
+        #print n, version, diff
+        nt.assert_almost_equal(diff, 0, places=12)
+
+    dt,cpu = solver(I, V, f, c, Lx, Ly, Nx, Ny, dt, T,
+                     user_action=assert_no_error,
+                     version='cython')
+    print "Time: %g" %cpu
+
+if __name__ == '__main__':
+    test_quadratic()
