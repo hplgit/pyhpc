08906697614567916244e857117ad48b808f712c
marenger <marenger@ulrik.uio.no>
2014-07-21 09:31:43 +0200

    updates

---
+++ doc/src/project/src/2Dwave/make_wave2D_u0.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Compile extension modules for the loop
+
+
+# Cython (easier with pyximport)
+module=wave2D_u0_loop_cy
+rm -f $module.so
+python setup_${module}.py build_ext --inplace   # compile
+python -c "import $module"                      # test
+if [ $? -eq 0 ]; then                           # success?
+  echo "Cython module $module successfully built"
+else
+  echo "Building Cython module $module failed"
+  exit 1
+fi
+
+# Cython (easier with pyximport)
+module=wave2D_u0_par_loop_cy
+rm -f $module.so
+CC='gcc' python setup_${module}.py build_ext --inplace   # compile
+python -c "import $module"                      # test
+if [ $? -eq 0 ]; then                           # success?
+  echo "Cython module $module successfully built"
+else
+  echo "Building Cython module $module failed"
+  exit 1
+fi
+
+# Cython interface to C code
+module=wave2D_u0_loop_c_cy
+rm -f $module.so
+CC='gcc' python setup_${module}.py build_ext --inplace   # compile
+python -c "import $module"                      # test
+if [ $? -eq 0 ]; then                           # success?
+  echo "Cython module $module successfully built"
+else
+  echo "Building Cython module $module failed"
+  exit 1
+fi
